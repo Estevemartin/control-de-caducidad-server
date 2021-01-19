65482b55384b01af6290c27a4f699305264dc33f
Esteve Martin <esteve.martin@mpasolutions.es>
2021-01-19 10:18:29 +0100

    Project Start

---
+++ cfg/cloudinary-setup.js
@@ -0,0 +1,25 @@
+const cloudinary = require('cloudinary').v2;
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
+const multer = require('multer');
+
+cloudinary.config({
+  cloud_name: process.env.cloudName,
+  api_key: process.env.cloudKey,
+  api_secret: process.env.cloudSecret
+});
+
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: 'gallery',
+    allowedFormats: ['jpg', 'png'],
+    filename: function (req, res, cb) {
+      let fileName = res.originalname.split(".")
+      return fileName[0]
+      // format: async (req, file) => 'png', // supports promises as well
+      // public_id: (req, file) => 'computed-filename-using-request',
+    },
+}});
+ 
+const uploader = multer({ storage });
+module.exports = uploader;
